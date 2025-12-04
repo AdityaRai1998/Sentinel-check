c88e24efd2d0b44b20e1069e7f00989232a3559e
Rai <adityrai@deloitte.com>
2025-12-04 10:23:47 +0530

    Updated code with new block

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
 provider "null" {}
 
-resource "null_resource" "example" {}
+variable "tags" {
+  type = map(string)
+  default = {
+    "Environment" = "dev"
+    "Owner"       = "example"
+    "Project"     = "playground"
+  }
+}
+
+resource "null_resource" "dummy_with_tags" {
+  # Provisioner is not required, just showing usage of tags
+  triggers = var.tags
+}
+
+output "applied_tags" {
+  value = null_resource.dummy_with_tags.triggers
+}
